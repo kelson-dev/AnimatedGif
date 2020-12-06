a92bd8cf3c1b7e7c7669b1c655dfe478166ef52c
Kelson Ball <kelson.ball@hey.com>
2020-12-06 10:59:34 -0800

    Formatting and update

--- AnimatedGif.Demo/Program.cs
+++ AnimatedGif.Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using AnimatedGif;
 
 namespace AnimatedGif.Demo
 {
@@ -7,15 +8,13 @@ namespace AnimatedGif.Demo
         public static void Main(string[] args)
         {
             // 33ms delay (~30fps)
-            using (var gif = AnimatedGif.Create("gif.gif", 33))
-            {
-                var img1 = Image.FromFile("img1.png");
-                gif.AddFrame(img1, delay: -1, quality: GifQuality.Bit8);
-                var img2 = Image.FromFile("img2.png");
-                gif.AddFrame(img2, delay: -1, quality: GifQuality.Bit8);
-                var img3 = Image.FromFile("img3.png");
-                gif.AddFrame(img3, delay: -1, quality: GifQuality.Bit8);
-            }
+            using var gif = AnimatedGif.Create("gif.gif", 33);
+            var img1 = Image.FromFile("Img1.png");
+            gif.AddFrame(img1, delay: -1, quality: GifQuality.Bit8);
+            var img2 = Image.FromFile("Img2.png");
+            gif.AddFrame(img2, delay: -1, quality: GifQuality.Bit8);
+            var img3 = Image.FromFile("Img3.png");
+            gif.AddFrame(img3, delay: -1, quality: GifQuality.Bit8);
         }
     }
 }
